9a52504f5c777b087fcaa70685db379f823e66ba
Michael Phelps <mpphelps@gmail.com>
2023-03-18 12:15:37 -0400

    Added basic lighting shader and two cubes demontstrating shader

--- PeanutGameEngine/Source/Objects/cubes.cpp
+++ PeanutGameEngine/Source/Objects/cubes.cpp
@@ -2,23 +2,7 @@
 
 namespace peanut {
 	namespace objects {
-		void Cubes::AddCube(glm::vec3 position)
-		{
-			positions.push_back(position);
-		}
-		glm::vec3 Cubes::GetPosition(int index) {
-			if (index >= positions.size() || index < 0) {
-				return glm::vec3(0.0f, 0.0f, 0.0f);
-				std::cout << "Cubes index out of bounds" << std::endl;
-			}
-			else {
-				return positions[index];
-			}
-			
-		}
-		int Cubes::Count() {
-			return positions.size();
-		}
+
 	}
 }
 
--- PeanutGameEngine/main.cpp
+++ PeanutGameEngine/main.cpp
@@ -26,113 +26,138 @@ int main()
 	using namespace maths;
 	using namespace objects;
 
-	Window window("Peanut!", SCR_WIDTH, SCR_HEIGHT);
-	Shader shader("Source/Shaders/basicClip.vert", "Source/Shaders/basicClip.frag");
-	Texture texture1("Source/Textures/container.jpg", false);
-	Texture texture2("Source/Textures/awesomeface.png", true);
-	Camera camera;
-	Cubes cubes;
-	cubes.AddCube(glm::vec3(0.0f, 0.0f, 0.0f));
-	cubes.AddCube(glm::vec3(0.0f, 0.0f, 0.0f));
-	cubes.AddCube(glm::vec3(2.0f, 5.0f, -15.0f));
-	cubes.AddCube(glm::vec3(-1.5f, -2.2f, -2.5f));
-	cubes.AddCube(glm::vec3(-3.8f, -2.0f, -12.3f));
-	cubes.AddCube(glm::vec3(2.4f, -0.4f, -3.5f));
-	cubes.AddCube(glm::vec3(-1.7f, 3.0f, -7.5f));
-	cubes.AddCube(glm::vec3(1.3f, -2.0f, -2.5f));
-	cubes.AddCube(glm::vec3(1.5f, 2.0f, -2.5f));
-	cubes.AddCube(glm::vec3(1.5f, 0.2f, -1.5f));
-	cubes.AddCube(glm::vec3(-1.3f, 1.0f, -1.5f));
-	cubes.AddCube(glm::vec3(1.1f, -2.0f, -2.5f));
-	cubes.AddCube(glm::vec3(3.2f, -2.0f, -2.5f));
-	cubes.AddCube(glm::vec3(4.3f, -2.0f, -2.5f));
-	cubes.AddCube(glm::vec3(6.4f, -2.0f, -2.5f));
-	cubes.AddCube(glm::vec3(8.5f, -2.0f, -2.5f));
-	cubes.AddCube(glm::vec3(10.6f, -2.0f, -2.5f));
-	cubes.AddCube(glm::vec3(12.7f, -2.0f, -2.5f));
-	cubes.AddCube(glm::vec3(14.3f, -2.0f, -2.5f));
-
 	glEnable(GL_DEPTH_TEST);
+
+	Window window("Peanut!", SCR_WIDTH, SCR_HEIGHT);
+	Shader lightingShader("Source/Shaders/lightingShader.vert", "Source/Shaders/lightingShader.frag");
+	Shader lightSourceShader("Source/Shaders/lightSourceShader.vert", "Source/Shaders/lightSourceShader.frag");
+	//Texture texture1("Source/Textures/container.jpg", false);
+	//Texture texture2("Source/Textures/awesomeface.png", true);
+	float vertices[] = {
+		-0.5f, -0.5f, -0.5f,
+		 0.5f, -0.5f, -0.5f,
+		 0.5f,  0.5f, -0.5f,
+		 0.5f,  0.5f, -0.5f,
+		-0.5f,  0.5f, -0.5f,
+		-0.5f, -0.5f, -0.5f,
+
+		-0.5f, -0.5f,  0.5f,
+		 0.5f, -0.5f,  0.5f,
+		 0.5f,  0.5f,  0.5f,
+		 0.5f,  0.5f,  0.5f,
+		-0.5f,  0.5f,  0.5f,
+		-0.5f, -0.5f,  0.5f,
+
+		-0.5f,  0.5f,  0.5f,
+		-0.5f,  0.5f, -0.5f,
+		-0.5f, -0.5f, -0.5f,
+		-0.5f, -0.5f, -0.5f,
+		-0.5f, -0.5f,  0.5f,
+		-0.5f,  0.5f,  0.5f,
+
+		 0.5f,  0.5f,  0.5f,
+		 0.5f,  0.5f, -0.5f,
+		 0.5f, -0.5f, -0.5f,
+		 0.5f, -0.5f, -0.5f,
+		 0.5f, -0.5f,  0.5f,
+		 0.5f,  0.5f,  0.5f,
+
+		-0.5f, -0.5f, -0.5f,
+		 0.5f, -0.5f, -0.5f,
+		 0.5f, -0.5f,  0.5f,
+		 0.5f, -0.5f,  0.5f,
+		-0.5f, -0.5f,  0.5f,
+		-0.5f, -0.5f, -0.5f,
+
+		-0.5f,  0.5f, -0.5f,
+		 0.5f,  0.5f, -0.5f,
+		 0.5f,  0.5f,  0.5f,
+		 0.5f,  0.5f,  0.5f,
+		-0.5f,  0.5f,  0.5f,
+		-0.5f,  0.5f, -0.5f,
+	};
+	// lighting
+	glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
 
 	// It is advised to first do scaling operations, then rotations and lastly translations 
 	// when combining matrices otherwise they may (negatively) affect each other.
 
-	GLuint VBO, VAO, EBO;
-	glGenVertexArrays(1, &VAO);
+	GLuint VBO, cubeVAO;
+	glGenVertexArrays(1, &cubeVAO);
 	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
-	// bind the vertex array object first, then bind and set the vertex buffers, and then configure the vertex attributes.
-	glBindVertexArray(VAO);
-
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(cubes.vertices), cubes.vertices, GL_STATIC_DRAW);
-
-	/*glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);*/
-	
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// bind the vertex array object first, then bind and set the vertex buffers, and then configure the vertex attributes.
+	glBindVertexArray(cubeVAO);
 	// position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	// texture coord attribute
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
+
+	GLuint lightSourceVAO;
+	glGenVertexArrays(1, &lightSourceVAO);
+	glBindVertexArray(lightSourceVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
 	
 	// uncomment this call to draw in wireframe polygons.
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-	shader.use();
-	shader.setUniformMat1i("texture1", 0);
-	shader.setUniformMat1i("texture2", 1);
-	shader.setUniformMat1f("mixture", 0.5f);	
 
 	while (!window.closed()) {
 
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		window.cameraUpdate();
 		window.calculateFramerate();
 
 		window.clear();
 
-		texture1.bind(GL_TEXTURE0);
-		texture2.bind(GL_TEXTURE1);
+		//texture1.bind(GL_TEXTURE0);
+		//texture2.bind(GL_TEXTURE1);
 		
-		// ****** first transformation ******
-		glm::mat4 projection = glm::mat4(1.0f);
-		projection = glm::perspective(glm::radians(window.camera.Getfov()), static_cast<float>((window.getWidth() / window.getHeight())), 0.1f, 100.0f);
-		// ****** third transformation ******
+		lightingShader.use();
+		lightingShader.setUniformMat3f("objectColor", glm::vec3(1.0f, 0.5f, 0.31f));
+		lightingShader.setUniformMat3f("lightColor", glm::vec3(1.0f, 1.0f, 1.0f));
+
+		// ****** view/projection/model transformation ******
+		glm::mat4 projection = glm::perspective(glm::radians(window.camera.Getfov()), static_cast<float>((window.getWidth() / window.getHeight())), 0.1f, 100.0f);
 		glm::mat4 view = window.camera.View();
+		glm::mat4 model = glm::mat4(1.0f);
 		// set shader uniform
-		shader.setUniformMat4("view", view);
-		shader.setUniformMat4("projection", projection);
+		lightingShader.setUniformMat4("projection", projection);
+		lightingShader.setUniformMat4("view", view);
+		lightingShader.setUniformMat4("model", model);
+		
+		//render cube
+		glBindVertexArray(cubeVAO);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 
-		//render boxes
-		glBindVertexArray(VAO);
-		for (unsigned int i = 0; i < cubes.Count(); i++)
-		{
-			// ****** second transformation ******
-			glm::mat4 model = glm::mat4(1.0f);
-			model = translate(model, cubes.GetPosition(i));
-			float angle = 20.0f * (i + 1) * (float)glfwGetTime();
-			model = rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+		// draw the lamp object
+		lightSourceShader.use();
+		lightSourceShader.setUniformMat4("projection", projection);
+		lightSourceShader.setUniformMat4("view", view);
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, lightPos);
+		model = glm::scale(model, glm::vec3(0.2));
+		lightSourceShader.setUniformMat4("model", model);
 
-			// set shader uniform
-			shader.setUniformMat4("model", model);
-
-			// draw contain
-			glDrawArrays(GL_TRIANGLES, 0, 36);
-		}
-
+		//render cube
+		glBindVertexArray(lightSourceVAO);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 		
 		window.update();
 
 	}
 
-	glDeleteVertexArrays(1, &VAO);
+	glDeleteVertexArrays(1, &cubeVAO);
+	glDeleteVertexArrays(1, &lightSourceVAO);
 	glDeleteBuffers(1, &VBO);
-	glDeleteBuffers(1, &EBO);
-	shader.disable();
-	shader.~Shader();
-	texture1.~Texture();
-	texture2.~Texture();
+	lightingShader.disable();
+	lightingShader.~Shader();
+	lightSourceShader.disable();
+	lightSourceShader.~Shader();
+	//texture1.~Texture();
+	//texture2.~Texture();
 	glfwTerminate();
 	return 0;
 }
